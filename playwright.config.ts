1f5301867d8fbf7d3ca69e6b27724ea5920f13f9
s94 <mr.slater.94@gmail.com>
2022-04-25 09:51:28 +0100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   testDir: './test',
   timeout: 5000,
-  workers: 5
+  workers: 1
 }
 
 export default config
